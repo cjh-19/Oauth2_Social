0d38803431f3485657118933abfd63f13d648342
cjh-19 <gns8812@naver.com>
2024-01-22 23:57:45 +0900

    RestController test

---
+++ oauth2test/src/main/java/com/example/oauth2test/HttpControllertest.java
@@ -0,0 +1,14 @@
+package com.example.oauth2test;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// 사용자가 요청 -> 응답(HTML 파일)
+// @Controller
+
+// 시용자가 요청 -> 응답(Data)
+@RestController
+public class HttpControllertest {
+    public String getTest(){
+        return null;
+    }
+}
