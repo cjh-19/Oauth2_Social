4398a6572413b36aaec5236fc2a84e174537680f
cjh-19 <gns8812@naver.com>
2024-01-27 02:40:00 +0900

    modification

--- oauth2test/src/main/java/com/example/oauth2test/controller/IndexController.java
+++ oauth2test/src/main/java/com/example/oauth2test/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.oauth2test.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴!
 public class IndexController {
