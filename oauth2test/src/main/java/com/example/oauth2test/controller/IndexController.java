61082df914be2221f0847a41bbef9cdb25c1ec56
cjh-19 <gns8812@naver.com>
2024-01-28 23:43:57 +0900

    add project

--- oauth2test/src/main/java/com/example/oauth2test/controller/IndexController.java
+++ oauth2test/src/main/java/com/example/oauth2test/controller/IndexController.java
@@ -10,10 +10,40 @@ public class IndexController {
     // localhost:8080/
     // localhost:8080
     @GetMapping({"","/"})
-    public String index(){
+    public @ResponseBody String index(){
         // 머스테치 기본 폴더 src/main/resources/
         // 뷰리졸버 설정: templates (prefix), .mustache (suffix) 생략 가능!
 
         return "index"; // src/main/resources/templates/index.html
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료";
     }
 }
