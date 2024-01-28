80a65363afe2f968f1be27e6447b4db89e1c3868
cjh-19 <gns8812@naver.com>
2024-01-29 00:24:10 +0900

    need modification deprecated code

--- socialLogin/src/main/java/com/example/sociallogin/config/SecurityConfig.java
+++ socialLogin/src/main/java/com/example/sociallogin/config/SecurityConfig.java
@@ -1,9 +1,43 @@
 package com.example.sociallogin.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+//import com.example.sociallogin.config.oauth.PrincipalOauth2UserService;
+
+@Configuration // IoC 빈(bean)을 등록
 public class SecurityConfig {
+
+    //@Autowired
+    //private PrincipalOauth2UserService principalOauth2UserService;
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // CSRF 보호 비활성화
+                .authorizeRequests(authz -> authz // 요청에 대한 권한 설정
+                        .antMatchers("/user/**").authenticated()
+                        .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                        .anyRequest().permitAll())
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/login")
+                .userInfoEndpoint();
+        //.userService(principalOauth2UserService);
+
+        return http.build();
+    }
 }
