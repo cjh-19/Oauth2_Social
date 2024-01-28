1af9917c0c5d2d763a21d412f86af684b138e2d7
cjh-19 <gns8812@naver.com>
2024-01-29 01:43:38 +0900

    modification

--- socialLogin/src/main/java/com/example/sociallogin/config/SecurityConfig.java
+++ socialLogin/src/main/java/com/example/sociallogin/config/SecurityConfig.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-//import com.example.sociallogin.config.oauth.PrincipalOauth2UserService;
+// import com.example.sociallogin.config.oauth.PrincipalOauth2UserService;
 
 @Configuration // IoC 빈(bean)을 등록
 public class SecurityConfig {
 
-    //@Autowired
-    //private PrincipalOauth2UserService principalOauth2UserService;
+    // @Autowired
+    // private PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean
     public BCryptPasswordEncoder encodePwd() {
@@ -22,21 +24,42 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable() // CSRF 보호 비활성화
-                .authorizeRequests(authz -> authz // 요청에 대한 권한 설정
-                        .antMatchers("/user/**").authenticated()
-                        .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                        .anyRequest().permitAll())
+        //http.csrf().disable();
+        http.csrf(CsrfConfigurer::disable);
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/user/**").authenticated()
+                // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
+                // hasRole('ROLE_USER')")
+                // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
+                // hasRole('ROLE_USER')")
+                .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/loginProc")
                 .defaultSuccessUrl("/")
-                .and()
                 .oauth2Login()
                 .loginPage("/login")
-                .userInfoEndpoint();
-        //.userService(principalOauth2UserService);
+                .userInfoEndpoint()
+                //.userService(principalOauth2UserService);
+        );
+//                .requestMatchers("/user/**").authenticated()
+//                // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
+//                // hasRole('ROLE_USER')")
+//                // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
+//                // hasRole('ROLE_USER')")
+//                .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+//                .anyRequest().permitAll()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/loginProc")
+//                .defaultSuccessUrl("/")
+//                .and()
+//                .oauth2Login()
+//                .loginPage("/login")
+//                .userInfoEndpoint();
+//                //.userService(principalOauth2UserService);
 
         return http.build();
     }
