a438a5a4c8ec294c0396b3a3b9fedd2c2bd7ffb3
cjh-19 <gns8812@naver.com>
2024-02-01 23:56:38 +0900

    securityconfig update

--- socialLogin/src/main/java/com/example/sociallogin/config/SecurityConfig.java
+++ socialLogin/src/main/java/com/example/sociallogin/config/SecurityConfig.java
@@ -4,18 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-// import com.example.sociallogin.config.oauth.PrincipalOauth2UserService;
+//import com.cos.securityex01.config.oauth.PrincipalOauth2UserService;
 
 @Configuration // IoC 빈(bean)을 등록
 public class SecurityConfig {
 
-    // @Autowired
-    // private PrincipalOauth2UserService principalOauth2UserService;
+    //@Autowired
+    //private PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean
     public BCryptPasswordEncoder encodePwd() {
@@ -24,44 +22,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //http.csrf().disable();
-        http.csrf(CsrfConfigurer::disable);
-        http.authorizeHttpRequests(authorize -> authorize
+        http.csrf().disable();
+        http.authorizeRequests()
                 .requestMatchers("/user/**").authenticated()
                 // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
                 // hasRole('ROLE_USER')")
                 // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
                 // hasRole('ROLE_USER')")
                 .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                // https://deeplify.dev/back-end/spring/oauth2-social-login
-                // 위 링크에서 스프링 시큐리티 공부해서 소셜로그인 공부할 것
                 .anyRequest().permitAll()
+                .and()
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/loginProc")
                 .defaultSuccessUrl("/")
+                .and()
                 .oauth2Login()
                 .loginPage("/login")
-                .userInfoEndpoint()
-                //.userService(principalOauth2UserService);
-        );
-//                .requestMatchers("/user/**").authenticated()
-//                // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
-//                // hasRole('ROLE_USER')")
-//                // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
-//                // hasRole('ROLE_USER')")
-//                .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-//                .anyRequest().permitAll()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .loginProcessingUrl("/loginProc")
-//                .defaultSuccessUrl("/")
-//                .and()
-//                .oauth2Login()
-//                .loginPage("/login")
-//                .userInfoEndpoint();
-//                //.userService(principalOauth2UserService);
+                .userInfoEndpoint();
+                ///.userService(principalOauth2UserService);
 
         return http.build();
     }
